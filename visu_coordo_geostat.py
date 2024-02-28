88c74d03f59c00714e6252d706f328fe6da32dac
zartwilly <zartwilly@gmail.com>
2024-02-28 19:10:33 +0000

    visu_geojson: plot the field by their coordinates.

--- visu_coordo_geostat.py
+++ visu_coordo_geostat.py
@@ -7,10 +7,12 @@ Created on Wed Feb 28 16:29:43 2024
 """
 
 import os
+import json
 
 import pyproj
 import scipy
 import folium
+import geojson # pip install geojson
 import numpy as np
 import pandas as pd
 from scipy.spatial import ConvexHull
@@ -48,7 +50,9 @@ def read_csv(data_path="projet_geo_assouinde", nom_site="assouande3parcelles_tes
     
     
     # Make an empty map
-    fl_n = folium.Map(location=[3,-5], tiles="OpenStreetMap", zoom_start=10)
+    fl_n = folium.Map(location=[5.2779,-3.8764], 
+                      tiles="OpenStreetMap", 
+                      zoom_start=10)
     
     for name in names:
         data_df = df.loc[(name,),]
@@ -105,7 +109,149 @@ def read_csv(data_path="projet_geo_assouinde", nom_site="assouande3parcelles_tes
     
     pass
 
+def add_lat_lon_to_dataframe(data_path="projet_geo_assouinde", 
+                             nom_site="assouande3parcelles_test.csv"):
+    data_csv = os.path.join( data_path, "data", nom_site)
+    df = pd.read_csv(data_csv, index_col=[0,1], sep=";", header=1)
+    df = pd.read_csv(data_csv, sep=";", header=1)
+    df = df.ffill()
+    
+    df.set_index(['Name', 'Coord'], inplace=True)
+    
+    names = list()
+    coords = list()
+    for name, coord in df.index:
+        X = df.loc[(name,coord),"X"]
+        Y = df.loc[(name,coord),"Y"]
+        lat, lon =  convert_XY_to_latlon((X,Y))
+        df.loc[(name,coord),"Lat"] = lat
+        df.loc[(name,coord),"Lon"] = lon
+        names.append(name)
+        coords.append(coord)
+        
+    return df, names, coords
 
+def creation_geojson_file(data_path="projet_geo_assouinde", 
+                          nom_site="assouande3parcelles_test.csv",
+                          data_geo_save="data_geojson"):
+    
+    df, names, coords = add_lat_lon_to_dataframe(data_path, nom_site)
+    """
+    liste_coords = [[-3.8759994466211367,5.278300354992908],
+                    [-3.875881343998543,5.278440244239576],
+                    [-3.8757756231030953,5.278360104301768],
+                    [-3.87590163941897,5.278211640433852],
+                    [-3.8759994466211367,5.278300354992908]]
+    id_x = "0"
+    name_parcelle = "Parcelle"
+    source_geojson = f'{"type":"FeatureCollection", "features":[{"type":"Feature","properties":"{}",
+                        "name":{name_parcelle},
+                        "geometry":{"coordinates":{liste_coords},"type":"LineString"},"id":{id_x}}]}'
+                    
+    a1 = '{"type":"FeatureCollection", "features":[{"type":"Feature","properties":"{}",'
+    a2 = f'"name":{name_parcelle},'
+    c1 = f'"coordinates":{liste_coords},"type":"LineString"'
+    g1 = '"geometry":{'+c1+'},'
+    i1 = "id:{"+ id_x +"}]}"
+    source_geojson = a1+a2+g1+i1
+    """
+    # Define WGS84 as CRS:
+    geod = pyproj.Geod('+a=6378137 +f=0.003_352_810_664_747_512_6')
+    
+    geo = []
+    for idx, name_parcelle in enumerate(names):
+        data_df = df.loc[(name_parcelle,),]
+        
+        lats = np.array(data_df.loc[:,"Lat"])
+        lons = np.array(data_df.loc[:,"Lon"])
+        
+        # Compute:
+        area, perim = geod.polygon_area_perimeter(lons, lats)
+        
+        dico = dict()
+        coords = list(data_df.index)
+        liste_coords = list()
+        for coord in coords:
+            Lat = df.loc[(name_parcelle,coord),"Lat"]
+            Lon = df.loc[(name_parcelle,coord),"Lon"]
+            liste_coords.append([Lat, Lon])
+            
+        geometry = {"coordinates": liste_coords, "type":"LineString"}
+        
+        feature = {"type":"Feature","properties":"{}","name": name_parcelle,
+                   "geometry": geometry, "id": idx, "area": area, 
+                   "perim": perim}
+        
+        #geo = dict()
+        geo = {"type":"FeatureCollection", "features":[feature]}
+        with open(f'{data_geo_save}/{name_parcelle}.geojson', 'w') as fp:
+            json.dump(geo, fp)
+        
+    pass
+
+def create_visu_geojson(data_path="projet_geo_assouinde",
+                          data_geo_save="data_geojson", 
+                          name_parcelle="P"):
+    
+    # Geojson data
+    path = os.path.join( data_path, data_geo_save)
+    number_files =  len(os.listdir(path))
+    parcelles = []
+    for i in range(1, number_files+1):
+        parcelles.append(os.path.join(data_geo_save, f"{name_parcelle}{i}.geojson"))
+        
+    # Define WGS84 as CRS:
+    geod = pyproj.Geod('+a=6378137 +f=0.003_352_810_664_747_512_6')
+
+    # Define folium object
+    fl_n = folium.Map(location=[5.17564,-3.47634], tiles="OpenStreetMap", 
+                      zoom_start=25)
+    
+    for i, parcelle in enumerate(parcelles):
+        with open(parcelle) as f:
+            gj = geojson.load(f)
+        name = gj["features"][0]["name"]
+        df_data = pd.DataFrame(gj["features"][0]["geometry"]["coordinates"], 
+                               columns=["lat","lon"])
+        
+        # Compute:
+        area, perim = geod.polygon_area_perimeter(df_data.lon, df_data.lat)
+        area = round(abs(area), 3)
+        perim = round(abs(perim), 3)
+        
+        gj["features"][0]["area"] = area
+        
+        html=f"""
+            <h1> {name}</h1>
+            <p>infos de la parcelle </p>
+            <ul>
+                <li>Surface: {area}</li>
+                <li>Perimetre: {perim}</li>
+                
+            </ul>
+            </p>
+            <p>And that's a <a href="https://python-graph-gallery.com">link</a></p>
+            """
+        iframe = folium.IFrame(html=html, width=200, height=200)
+        
+        popup = folium.Popup(iframe, max_width=2650)
+        
+        folium.GeoJson( gj,
+            popup=popup,
+            highlight_function=lambda feature: {
+                "fillColor": (
+                    "green" if feature["area"] < 400 else "#ffff00"
+                ),
+            },
+        ).add_to(fl_n)
+        
+
+    # Save the map again
+    name_file = nom_site.split(".")[0]
+    #fl_n.save("folium_geojson.html")
+    fl_n.save(f"visu_folium_geojson_{name_file}.html")
+
+    pass
 
 if __name__ == "__main__":
     
@@ -113,7 +259,11 @@ if __name__ == "__main__":
     nom_site = "assouande15parcelles.csv"
     nom_site = "assouande42parcelles.csv"
     data_path = "." # "geoVisu_Assouinde"
-    read_csv(data_path=data_path, nom_site=nom_site)
+    #read_csv(data_path=data_path, nom_site=nom_site)
     
+    creation_geojson_file(data_path=data_path, nom_site=nom_site)
+    create_visu_geojson(data_path=".",
+                        data_geo_save="data_geojson", 
+                        name_parcelle="P")
     pass
     
