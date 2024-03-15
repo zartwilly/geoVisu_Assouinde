ea8431065f1571a03e0426201ece9950211fa4f4
zartwilly <zartwilly@gmail.com>
2024-03-15 18:44:28 +0000

    test visu: test for creating a visualisation a polygon

--- test_visu_geojsonPolygon.py
+++ test_visu_geojsonPolygon.py
@@ -145,6 +145,63 @@ def turn_file_from_csv_to_geojson(csv_filename, geojson_filename):
     
     return path_filename
 
+###############################################################################
+#               test geojson functions : start
+###############################################################################
+def turn_file_from_csv_to_geojson_NEW(csv_filename, geojson_filename):
+
+    data_csv = os.path.join( rootdata, csv_filename)
+    
+    df = pd.read_csv(data_csv, index_col=[0,1], sep=";", header=1)
+    df = pd.read_csv(data_csv, sep=";", header=1)
+    df = df.ffill()
+    
+    df.set_index(['Name', 'Coord'], inplace=True)
+    
+    df = compute_geostat_coordinate(df)
+    
+    #index_names = df.index.names # ---> get the name of index columns
+    names = df.index.get_level_values(level=0)
+    names = set(names)
+    
+    features = list()
+    for name_parcelle in names:
+        name_df = df.loc[(name_parcelle,),]
+        lats = np.array(name_df.loc[:,"Lat"])
+        lons = np.array(name_df.loc[:,"Lon"])
+        # Define WGS84 as CRS:
+        geod = pyproj.Geod('+a=6378137 +f=0.003_352_810_664_747_512_6')
+        # Compute:
+        area, perim = geod.polygon_area_perimeter(lons, lats)
+        
+        coordinates, properties \
+            = get_geosjon_properties_coordinates(name_df, name_parcelle, area, perim)
+        
+        
+        feature = {"type": "Feature", "properties": properties,
+                   "geometry": {"type": "LineString", "coordinates": coordinates}}
+                   #"geometry": {"type": "Polygon", "coordinates": coordinates}}
+        
+        features.append(feature)
+        
+    #geom_in_geojson = geojson.Feature(geometry=features, properties={})
+    geojson_dico = geojson.FeatureCollection(features=features)
+    # geojson_dico = {"type": "FeatureCollection",
+    #                 "crs": { "type": "name", 
+    #                         "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } 
+    #                         },
+    #                 "features": features}
+    
+    
+    path_filename = os.path.join(rootdata_geo, geojson_filename)
+    with open(path_filename,'w') as f:
+        json.dump(geojson_dico, f)
+    
+    return path_filename
+###############################################################################
+#               test geojson functions : end
+###############################################################################
+
 def visu_geojson_polygon_assouinde(filename):
     fo_m = folium.Map(location=[5.2779,-3.8764], zoom_start=10)
     # fl_n = folium.Map(location=[5.2779,-3.8764], 
@@ -171,7 +228,12 @@ if __name__ == "__main__":
     csv_filename = filename+".csv" # assouande42parcelles.csv
     geojson_filename = filename+".geojson" # assouande42parcelles.geojson
     
-    path_filename = turn_file_from_csv_to_geojson(csv_filename, geojson_filename)
+    #path_filename = turn_file_from_csv_to_geojson(csv_filename, geojson_filename)
+    
+    #visu_geojson_polygon_assouinde(filename)
+    
+    
+    path_filename = turn_file_from_csv_to_geojson_NEW(csv_filename, geojson_filename)
     
     visu_geojson_polygon_assouinde(filename)
     
